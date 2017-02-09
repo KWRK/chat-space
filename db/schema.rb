197ead70a8744b30dafa6f1c46028048ddf918cf
keigo-ohkawara <keigo.ohkawara@gmail.com>
2017-02-09 13:55:53 +0900

    Change name column contraint

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 20170208080128) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name"
+    t.string   "name",                                null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
