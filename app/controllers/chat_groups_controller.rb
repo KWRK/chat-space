0247f47b081b4dac3ccd85ef148a1787cf451fc6
keigo-ohkawara <keigo.ohkawara@gmail.com>
2017-02-10 15:26:18 +0900

    Edit controller and routes

--- app/controllers/chat_groups_controller.rb
+++ app/controllers/chat_groups_controller.rb
@@ -1,7 +1,17 @@
 class ChatGroupsController < ApplicationController
   before_action :authenticate_user!
+  protect_from_forgery except: :ChatGroups_post
 
   def index
   end
 
+  def create
+    Group.create(group_params)
+    redirect_to action: :index
+  end
+
+  private
+  def group_params
+    params.permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   devise_for :users
   root "chat_groups#index"
+  post "chat_groups" => "chat_groups#create"
   get "chat_groups" => "chat_groups#index"
+  resources "groups" ,only: [:new, :edit]
 
 end
