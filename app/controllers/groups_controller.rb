6a99a7ea28c5a7eb444fa4b4b63a154b704da88b
keigo-ohkawara <keigo.ohkawara@gmail.com>
2017-02-11 15:31:12 +0900

    Change controller name

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,30 @@
+class GroupsController < ApplicationController
+  before_action :authenticate_user!
+  protect_from_forgery except: :ChatGroups_post
+
+  def index
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    group = Group.new(group_params)
+    if group.save
+      flash[:notice] = '新しいグループを作成しました'
+    else
+      flash[:alert] = 'グループの作成に失敗しました'
+    end
+    redirect_to action: :index
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root "chat_groups#index"
-  resources "chat_groups" ,only: [:new, :edit ,:create]
+  root "groups#index"
+  resources "groups" ,only: [:new, :edit ,:create]
 
 end
