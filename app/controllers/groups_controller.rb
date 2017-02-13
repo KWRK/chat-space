0499e696aef405c2a581b13284f2a2c0da118466
keigo-ohkawara <keigo.ohkawara@gmail.com>
2017-02-13 21:12:57 +0900

    Fix redirect path

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -9,8 +9,9 @@ class GroupsController < ApplicationController
   def show
     @group = Group.find(params[:id])
     @groups = current_user.groups.all
+    @users = Group.find(params[:id]).user.all
   end
-
+ 
   def new
     @group = Group.new
   end
@@ -18,7 +19,7 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     if @group.save
-      redirect_to root_path, notice:'新しいグループを作成しました'
+      redirect_to group_path(@group), notice:'新しいグループを作成しました'
     else
       flash[:alert] = 'グループの作成に失敗しました'
       render action: :new
@@ -32,11 +33,10 @@ class GroupsController < ApplicationController
   def update
     group = Group.find(params[:id])
     if group.update(group_params)
-      group.update(group_params)
       redirect_to group_path, notice:'グループ情報を更新しました'
     else
       flash[:alert] = 'グループ情報の更新に失敗しました'
-      render action: :edit
+      redirect_to edit_group_path
     end
   end
 
