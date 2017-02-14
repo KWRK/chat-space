6530293024f9af99f454345a8c21ec807e0ac363
keigo-ohkawara <keigo.ohkawara@gmail.com>
2017-02-14 16:34:40 +0900

    Create chats controller, models, association

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ApplicationRecord
   validates :name, :user, presence: true
-
+  has_many :chats
   has_many :groups_users
   accepts_nested_attributes_for :groups_users
   has_many :user, through: :groups_users
