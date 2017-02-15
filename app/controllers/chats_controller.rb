734fd8e49c324fe2ccbed3bab6f9d7fcff578550
keigo-ohkawara <keigo.ohkawara@gmail.com>
2017-02-15 11:23:59 +0900

    Create index action for chats

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,12 +1,13 @@
 class ChatsController < ApplicationController
 
+  def index
+    @group = Group.find(params[:id])
+    @chats = @group.chats
+  end
+
   def create
     @chat = Chat.create(chat_params)
     redirect_to group_path(@chat.group_id)
-
-  end
-
-  def show
   end
 
   private
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -28,7 +28,7 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:id]).order('update_at DESC')
+    @group = Group.find(params[:id])
   end
 
   def update
