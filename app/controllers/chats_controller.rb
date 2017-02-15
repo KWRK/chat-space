892d0c8dab668cb8b9c048b034429e40d022ad2f
keigo-ohkawara <keigo.ohkawara@gmail.com>
2017-02-15 18:39:43 +0900

    Add validates

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -9,6 +9,10 @@ class ChatsController < ApplicationController
   end
 
   def create
+    @groups = current_user.groups
+    @group = Group.find(params[:group_id])
+    @users = @group.user
+    @chats = @group.chats
     @chat = Chat.new(chat_params)
     if @chat.save
       redirect_to group_chats_path(@chat.group_id)
