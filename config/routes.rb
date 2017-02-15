f5169b681a863e9b6d724cf8b43e20d7f7aae798
keigo-ohkawara <keigo.ohkawara@gmail.com>
2017-02-15 11:24:36 +0900

    Add index routes for chats

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root "groups#index"
   resources "groups", except: :destroy do
-    resources "chats", only: :create
+    resources "chats", only: %i(index create)
   end
 end
