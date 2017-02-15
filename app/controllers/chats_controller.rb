039f8753ba99552adc118a4c9da1eaab9052ed36
keigo-ohkawara <keigo.ohkawara@gmail.com>
2017-02-15 17:49:57 +0900

    Create chats/show action

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,18 +1,21 @@
 class ChatsController < ApplicationController
 
   def index
-    @group = Group.find(params[:id])
+    @groups = current_user.groups
+    @group = Group.find(params[:group_id])
+    @users = @group.user
     @chats = @group.chats
+    @chat = Chat.new
   end
 
   def create
     @chat = Chat.create(chat_params)
-    redirect_to group_path(@chat.group_id)
+    redirect_to group_chats_path(@chat.group_id)
   end
 
   private
 
   def chat_params
-    params.permit(:body, :user_id, :group_id)
+    params.require(:chat).permit(:body, :user_id, :group_id)
   end
 end
