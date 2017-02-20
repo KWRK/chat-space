80f111abe5b414a17fc97030b113386943af048e
keigo-ohkawara <keigo.ohkawara@gmail.com>
2017-02-20 11:55:51 +0900

    Refine routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root "groups#index"
-  resources "groups", except: %i(destroy show) do
-    resources "chats", only: %i(index create)
+  resources :groups, except: %i(destroy show) do
+    resources :chats, only: %i(index create)
   end
 end
