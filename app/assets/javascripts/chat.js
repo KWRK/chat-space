fbe183e0fa832c95b8ec8fbe9c911a06f27e0e52
keigo-ohkawara <keigo.ohkawara@gmail.com>
2017-02-23 13:46:34 +0900

    Create chat.js

---
+++ app/assets/javascripts/chat.js
@@ -0,0 +1,61 @@
+$(function(){
+  $(function () {  // チャット一覧部分を常に一番下にスクロールして表示
+    var target = document.getElementById( "content__right__body" ) ;
+    var scrollHeight = target.scrollHeight ;
+    $("#content__right__body").scrollTop(scrollHeight);
+  });
+
+  function buildBodyHTML(data) {
+    var bodyHtml = data.body;
+    return bodyHtml;
+  }
+  function buildUserHTML(data) {
+    var userHtml = data.name;
+    return userHtml;
+  }
+  function buildTimeHTML(data) {
+    var timeHtml = data.time;
+    return timeHtml;
+  }
+
+  $(".new_chat").on('submit', function(e) { //submit時にメッセージ入力部のデータを取得
+    e.preventDefault();
+    var bodyField = $('.js-input');
+    var groupHidden = $('#chat_group_id')
+    var userHidden = $('#chat_user_id')
+    var body = bodyField.val();
+    var group = groupHidden.val();
+    var user = userHidden.val();
+    $.ajax({  //取得したデータにparamsの紐付けをして、./chats.jsonにPOST
+      type: 'POST',
+      url: './chats.json',
+      data: {
+        chat: {
+          body: body,
+          group_id: group,
+          user_id: user,
+        }
+      },
+    })
+    .done(function(data) {  //データが取得できたとき、定形にして出力してから入力部をカラにして、も一回スクロール
+      var bodyHtml = buildBodyHTML(data);
+      var userHtml = buildUserHTML(data);
+      var timeHtml = buildTimeHTML(data);
+      $('.content__right__body').append('<div class="js_chat-data"><ul><li id="js_user_name"></li><li id="js_create_time"></li></ul><div class="js_chat-body"></div></div>');
+      $('.js_chat-body').append(bodyHtml);
+      $('#js_user_name').append(userHtml);
+      $('#js_create_time').append(timeHtml);
+      bodyField.val('');
+      $(function () {  // 新しいチャットをフェードイン（っぽくみせる）
+        var target = document.getElementById( "content__right__body" ) ;
+        var scrollHeight = target.scrollHeight ;
+        $('#content__right__body').animate( { scrollTop:(scrollHeight) },1500 );
+      });
+      $('#content__right__body').append('<div id="js_chat-input__guard">続けてメッセージを送信するには<a href= "chats">リロード</a>して下さい。</div>'); //連続投稿できない事をつたえる表示
+      $('body').append('<div class="alert alert-success">メッセージを投稿しました</div>');
+    })
+    .fail(function() {  //なんか失敗した時用のエラー
+      $('body').append('<div class="alert alert-danger">メッセージを入力して下さい</div>');
+    });
+  });
+});
