671b827f9ee9a077d2ed9e1e68a509297714a047
keigo-ohkawara <keigo.ohkawara@gmail.com>
2017-03-01 20:53:48 +0900

    Create image_uploader

---
+++ app/uploeders/image_uploader.rb
@@ -0,0 +1,23 @@
+class ImageUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+  storage :file
+  process convert: 'jpg'
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+
+
+  process resize_to_limit: [300, 300]
+
+  # jpg,jpeg,gif,pngしか受け付けない
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
+
+  def filename
+    super.chomp(File.extname(super)) + '.jpg' if original_filename.present?
+  end
+
+end
